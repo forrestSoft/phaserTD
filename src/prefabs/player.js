a49e20bb2fbd975fb4087b50319315956f164283
jeff forrester <jeff.forrester@gmail.com>
2017-03-23 22:51:07 -0400

    (almost) working reference implementation of pathfinding example

---
+++ src/prefabs/player.js
@@ -0,0 +1,70 @@
+import Prefab from './prefab'
+
+export default class extends Prefab {
+    constructor (game_state, name, position, properties) {
+        super(game_state, name, position, properties);
+        
+        //this.anchor.setTo(0.5);
+        //this.scale.setTo(0.666, 0.5);
+        
+        this.walking_speed = +properties.walking_speed;
+
+        this.game_state.game.physics.arcade.enable(this);
+        // change the size and position of the collision box
+        this.body.setSize(12, 12, 0, 4);
+        this.body.collideWorldBounds = true;
+        
+        // set anchor point to be the center of the collision box
+        this.anchor.setTo(0.5, 0.75);
+        
+        this.path = [];
+        this.path_step = -1;
+    }
+
+    update () {
+        let next_position, velocity;
+        this.game_state.game.physics.arcade.collide(this, this.game_state.layers.collision);
+        
+        if (this.path.length > 0) {
+            next_position = this.path[this.path_step];
+
+            if (!this.reached_target_position(next_position)) {
+                velocity = new Phaser.Point(next_position.x - this.position.x,
+                                       next_position.y - this.position.y);
+                velocity.normalize();
+                this.body.velocity.x = velocity.x * this.walking_speed;
+                this.body.velocity.y = velocity.y * this.walking_speed;
+            } else {
+                this.position.x = next_position.x;
+                this.position.y = next_position.y;
+                if (this.path_step < this.path.length - 1) {
+                    this.path_step += 1;
+                } else {
+                    this.path = [];
+                    this.path_step = -1;
+                    this.body.velocity.x = 0;
+                    this.body.velocity.y = 0;
+                }
+            }
+        }
+    }
+
+    reached_target_position (target_position) {
+        let distance;
+        distance = Phaser.Point.distance(this.position, target_position);
+        return distance < 1;
+    }
+
+    move_to (target_position) {
+        this.game_state.pathfinding.find_path(this.position, target_position, this.move_through_path, this);
+    }
+
+    move_through_path (path) {
+        if (path !== null) {
+            this.path = path;
+            this.path_step = 0;
+        } else {
+            this.path = [];
+        }
+    }
+}
--- src/states/Game.js
+++ src/states/Game.js
@@ -3,9 +3,14 @@ import Phaser from 'phaser'
 import Mushroom from '../sprites/Mushroom'
 import Pathfinding from '../pathfinding/pathfinding'
 
+import Prefab from '../prefabs/prefab'
+import Player from '../prefabs/player'
+
 export default class extends Phaser.State {
   init () {
-    console.log(this, this.cache.getJSON('level1'));
+    this.prefab_classes = {
+        "player": Player
+    };
 
     var tileset_index, tile_dimensions;
     this.level_data = this.cache.getJSON('level1');
@@ -28,12 +33,75 @@ export default class extends Phaser.State {
 
     // initialize pathfinding
     tile_dimensions = new Phaser.Point(this.map.tileWidth, this.map.tileHeight);
-    // debugger
     this.pathfinding = this.game.plugins.add(Pathfinding, this.map.layers[1].data, [-1], tile_dimensions);
   }
+
   preload () {}
 
+  create_object (object) {
+    let object_y, position, prefab;
+    // tiled coordinates starts in the bottom left corner
+    object_y = (object.gid) ? object.y - (this.map.tileHeight / 2) : object.y + (object.height / 2);
+    position = {"x": object.x + (this.map.tileHeight / 2), "y": object_y};
+    // create object according to its type
+    if (this.prefab_classes.hasOwnProperty(object.type)) {
+      prefab = new this.prefab_classes[object.type](this, object.name, position, object.properties);
+    }
+    this.prefabs[object.name] = prefab;
+  }
+
+  move_player () {
+    let target_position;
+    target_position = new Phaser.Point(this.game.input.activePointer.x, this.game.input.activePointer.y);
+    
+    this.prefabs.player.move_to(target_position);
+  }
+
+  render () {
+      this.game.debug.body(this.prefabs.player);
+  }
+
   create () {
+    let group_name, object_layer, collision_tiles;
+    
+    // create map layers
+    this.layers = {};
+    this.map.layers.forEach(function (layer) {
+        this.layers[layer.name] = this.map.createLayer(layer.name);
+        if (layer.properties.collision) { // collision layer
+            collision_tiles = [];
+            layer.data.forEach(function (data_row) { // find tiles used in the layer
+                data_row.forEach(function (tile) {
+                    // check if it's a valid tile index and isn't already in the list
+                    if (tile.index > 0 && collision_tiles.indexOf(tile.index) === -1) {
+                        collision_tiles.push(tile.index);
+                    }
+                }, this);
+            }, this);
+            this.map.setCollision(collision_tiles, true, layer.name);
+        }
+    }, this);
+    
+    // resize the world to be the size of the current layer
+    this.layers[this.map.layer.name].resizeWorld();
+    
+    // create groups
+    this.groups = {};
+    this.level_data.groups.forEach(function (group_name) {
+        this.groups[group_name] = this.game.add.group();
+    }, this);
+    
+    this.prefabs = {};
+    
+    for (object_layer in this.map.objects) {
+        if (this.map.objects.hasOwnProperty(object_layer)) {
+            // create layer objects
+            this.map.objects[object_layer].forEach(this.create_object, this);
+        }
+    }
+    
+    // add user input to move player
+    this.game.input.onDown.add(this.move_player, this);
     
     
 
