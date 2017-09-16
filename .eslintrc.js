843c01c0ecb6625ad1bc4c4c805b4dc1c0303519
Philip Blyth <philip.blyth@autodesk.com>
2017-09-15 21:35:52 -0400

    dev: adding ESLint and Prettier (for fire-and-forget code-consistency)

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+	extends: ['eslint:recommended', 'prettier', 'prettier/react'],
+	plugins: ['react', 'prettier'],
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaVersion: 2016,
+		sourceType: 'module',
+		ecmaFeatures: {
+			jsx: true
+		}
+	},
+	env: {
+		browser: true,
+		node: true,
+		es6: true
+	},
+	rules: {
+		curly: ['error', 'all'],
+		'prettier/prettier': [
+			'error',
+			{
+				printWidth: 120,
+				semi: false,
+				singleQuote: true,
+				trailingComma: 'none', // ideally 'all'...
+				tabWidth: 1,
+				useTabs: true
+			}
+		]
+	},
+	globals: {
+		// phaser: true,
+		game: true
+	},
+	settings: {
+		'import/core-modules': ['phaser', 'pixi', 'p2'],
+		'import/resolver': {
+			webpack: {
+				resolve: {
+					extensions: ['.js', '.jsx']
+				}
+			}
+		}
+	}
+}
